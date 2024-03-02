32bb1c466a83b1af26a7d73368fbae746d96e1c2
Bohdan Pavlov <bohdan.pavlov.o@gmail.com>
2024-03-02 18:38:37 +0200

    update from astro template

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -1,7 +1,7 @@
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend settig up the Lua Language Server (lua_ls) as this provides autocomplete and documentation while editing
-
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
@@ -11,9 +11,8 @@ return {
     features = {
       autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = off)
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       inlay_hints = false, -- enable/disable inlay hints on start
-      lsp_handlers = true, -- enable/disable setting of lsp_handlers
       semantic_tokens = true, -- enable/disable semantic token highlighting
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on

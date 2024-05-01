adf3292270f37adba015eba271ccd1b1d3388ce0
Bohdan Pavlov <bohdan.pavlov.o@gmail.com>
2024-05-02 01:03:07 +0300

    added sessionizer, and update btop.

---
+++ wezterm/config/hotkeys.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+local sessionizer = require("sessionizer")
+
+
+local Config = {}
+Config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+Config.keys = {
+    { key = "f", mods = "LEADER", action = wezterm.action_callback(sessionizer.toggle) },
+    --{ key = "F", mods = "LEADER", action = wezterm.action_callback(sessionizer.resetCacheAndToggle) },
+}
+
+return Config;
--- wezterm/config/init.lua
+++ wezterm/config/init.lua
@@ -2,6 +2,7 @@ return require("utils.fun").tbl_merge(
   (require "config.appearance"),
   (require "config.font"),
   (require "config.tab-bar"),
-  (require "config.general")
+  (require "config.general"),
+  (require "config.hotkeys")
 )
 

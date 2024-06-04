b7674f1f9e506bd298b9a81827866198b0d3df78
Bohdan Pavlov <bohdan.pavlov.o@gmail.com>
2024-06-04 23:24:53 +0300

    simplify wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,21 @@
 require "events.update-status"
 require "events.format-tab-title"
 require "events.new-tab-button-click"
---require "events.lock-interface"
 
-return require("utils.config"):new():add("config")
+
+local spec = require "config"
+local wez = require "wezterm"
+local config = wez.config_builder()
+config:set_strict_mode(true)
+
+for key, value in pairs(spec) do
+    if config[key] ~= nil then
+      wez.log_warn("Duplicate found: ", { old = config[key], new = spec[key] })
+    else
+      config[key] = value
+    end
+end
+
+return config
+
 
